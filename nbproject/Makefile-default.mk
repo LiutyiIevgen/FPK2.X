30d5998d65906095c0405eeeba3874f9a643826c
LiutyiIevgen <lutiyjeka@mail.ru>
2014-04-21 14:13:35 +0300

    Liutyi added InOutSignals

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=Can.c PathComands.c main.c
+SOURCEFILES_QUOTED_IF_SPACED=Can.c PathComands.c main.c InOutSignals.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/Can.o ${OBJECTDIR}/PathComands.o ${OBJECTDIR}/main.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/Can.o.d ${OBJECTDIR}/PathComands.o.d ${OBJECTDIR}/main.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/Can.o ${OBJECTDIR}/PathComands.o ${OBJECTDIR}/main.o ${OBJECTDIR}/InOutSignals.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/Can.o.d ${OBJECTDIR}/PathComands.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/InOutSignals.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/Can.o ${OBJECTDIR}/PathComands.o ${OBJECTDIR}/main.o
+OBJECTFILES=${OBJECTDIR}/Can.o ${OBJECTDIR}/PathComands.o ${OBJECTDIR}/main.o ${OBJECTDIR}/InOutSignals.o
 
 # Source Files
-SOURCEFILES=Can.c PathComands.c main.c
+SOURCEFILES=Can.c PathComands.c main.c InOutSignals.c
 
 
 CFLAGS=
@@ -100,6 +100,13 @@ ${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD3=1  -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
+${OBJECTDIR}/InOutSignals.o: InOutSignals.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/InOutSignals.o.d 
+	@${RM} ${OBJECTDIR}/InOutSignals.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  InOutSignals.c  -o ${OBJECTDIR}/InOutSignals.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/InOutSignals.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_ICD3=1  -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
+	@${FIXDEPS} "${OBJECTDIR}/InOutSignals.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
 else
 ${OBJECTDIR}/Can.o: Can.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
@@ -121,6 +128,13 @@ ${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/main.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d"      -g -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
+${OBJECTDIR}/InOutSignals.o: InOutSignals.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/InOutSignals.o.d 
+	@${RM} ${OBJECTDIR}/InOutSignals.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  InOutSignals.c  -o ${OBJECTDIR}/InOutSignals.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/InOutSignals.o.d"      -g -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
+	@${FIXDEPS} "${OBJECTDIR}/InOutSignals.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 endif
 
